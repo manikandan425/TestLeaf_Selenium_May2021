9efce44e6d16088cc48e0bdf8dbf7d1531e8880a
manikandan425 <83874959+manikandan425@users.noreply.github.com>
2021-05-08 21:13:37 +0530

    Add files via upload

---
+++ Week1/Day1/Assignments/Factorial.java
@@ -0,0 +1,15 @@
+package week1.day1.assignments;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input=5;
+		int fact=1;
+		for (int i = 1; i <= input; i++) {
+		fact=fact*i;		
+		}
+		System.out.println("factorial of "+input +" is 5*4*3*2*1=" +fact);	
+	}
+
+}
