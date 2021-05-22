8fa1480a1b75a079f33ab4cfc4344d9a365a8847
manikandan425 <83874959+manikandan425@users.noreply.github.com>
2021-05-23 01:10:45 +0530

    Add files via upload

---
+++ Week3/Day1/Computer/Computer.java
@@ -0,0 +1,8 @@
+package week3.day1.assignments;
+
+public class Computer {
+
+	public void computerModel() {
+		System.out.println("Print Computer Model");
+	}
+}
---
+++ Week3/Day1/Computer/Desktop.java
@@ -0,0 +1,16 @@
+package week3.day1.assignments;
+
+public class Desktop extends Computer{
+
+	public void desktopSize() {
+		System.out.println("Print Desktop size");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Desktop desk = new Desktop();
+		desk.computerModel();
+		desk.desktopSize();
+	}
+
+}
